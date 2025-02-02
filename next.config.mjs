2b3466ab6975e05b286eabef2dbe8ba866e3f40c
Muhodari <sagemuho@gmail.com>
2025-02-02 08:24:38 +0000

    Add Next.js configuration

---
+++ next.config.mjs
@@ -0,0 +1,47 @@
+let userConfig = undefined
+try {
+  userConfig = await import('./v0-user-next.config')
+} catch (e) {
+  // ignore error
+}
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    webpackBuildWorker: true,
+    parallelServerBuildTraces: true,
+    parallelServerCompiles: true,
+  },
+}
+
+if (userConfig) {
+  // ESM imports will have a "default" property
+  const config = userConfig.default || userConfig
+
+  for (const key in config) {
+    if (
+      typeof nextConfig[key] === 'object' &&
+      !Array.isArray(nextConfig[key])
+    ) {
+      nextConfig[key] = {
+        ...nextConfig[key],
+        ...config[key],
+      }
+    } else {
+      nextConfig[key] = config[key]
+    }
+  }
+}
+
+export default nextConfig
+
+// Simulated initial commit note on 2025-02-02T08:24:38.411Z

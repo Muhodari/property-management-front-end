8c53a76ecfa93f0b1c6a5658af7af563b54853fe
Muhodari <sagemuho@gmail.com>
2025-02-05 09:38:33 +0000

    Add theme provider component

---
+++ components/theme-provider.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import * as React from 'react'
+import {
+  ThemeProvider as NextThemesProvider,
+  type ThemeProviderProps,
+} from 'next-themes'
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}
+
+// Simulated initial commit note on 2025-02-05T09:38:33.646Z

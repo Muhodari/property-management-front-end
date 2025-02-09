a42385239c7bb99342738dbe8c1c7083059c105c
Muhodari <sagemuho@gmail.com>
2025-02-09 23:29:26 +0000

    Add toaster component

---
+++ components/ui/toaster.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { Toast, ToastClose, ToastDescription, ToastProvider, ToastTitle, ToastViewport } from "@/components/ui/toast"
+import { useToast } from "@/components/ui/use-toast"
+
+export function Toaster() {
+  const { toasts } = useToast()
+
+  return (
+    <ToastProvider>
+      {toasts &&
+        toasts.map(({ id, title, description, action, ...props }) => (
+          <Toast key={id} {...props}>
+            <div className="grid gap-1">
+              {title && <ToastTitle>{title}</ToastTitle>}
+              {description && <ToastDescription>{description}</ToastDescription>}
+            </div>
+            {action}
+            <ToastClose />
+          </Toast>
+        ))}
+      <ToastViewport />
+    </ToastProvider>
+  )
+}
+
+
+// Simulated initial commit note on 2025-02-09T23:29:26.499Z

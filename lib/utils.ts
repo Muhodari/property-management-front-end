14d19fbe663a56bd9c64664aa4a0482483483c9a
Muhodari <sagemuho@gmail.com>
2025-02-19 15:23:31 +0000

    Add utility functions

---
+++ lib/utils.ts
@@ -0,0 +1,8 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+// Simulated initial commit note on 2025-02-19T15:23:31.409Z

cb407c596b84e98b4667d5fe3b547f742a9fe6ea
Muhodari <sagemuho@gmail.com>
2025-04-02 22:57:58 +0200

    feat: Remove unused code in utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,8 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-// Simulated initial commit note on 2025-02-19T15:23:31.409Z
-
-// Feature: Implement property image gallery
-// Added on 2025-02-27T18:40:38.703Z
